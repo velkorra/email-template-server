da133f5a14a8081a7ecebd8369bc123cdb19785b
vertunoff <anton.s.vertunov@gmail.com>
2024-10-31 03:52:37 +0300

    feat: configure CORS

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,17 @@ import express from "express";
 import { initializeDatabase } from "./database/dbConfig";
 import { attachControllers } from "@decorators/express";
 import { UserController } from "./controllers/userController";
-var cookieParser = require("cookie-parser");
+import { CorsOptions } from "cors";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 const PORT: Number = 5000;
-const Root = "/";
+
+app.use(cors());
 app.use(json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
-// app.use("/", router);
 const middleware = (req: Request, res: Response, next: NextFunction) => {
     console.log(`${req.method} ${req.url}`);
     next();
